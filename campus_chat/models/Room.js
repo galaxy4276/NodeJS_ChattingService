508e21cd4a86a3151ae3b8fc852e5e7d74f6a980
galaxy <galaxyhi4276@gmail.com>
2020-11-16 23:51:53 +0900

    feat: Room 스키마 작성

---
+++ campus_chat/models/Room.js
@@ -0,0 +1,29 @@
+import { Model, DataTypes } from 'sequelize';
+
+export default class Room extends Model {
+  static init(sequelize) {
+    return super.init(
+      {
+        title: {
+          type: DataTypes.STRING(15),
+          allowNull: false,
+        },
+        count: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          defaultValue: 2,
+        },
+        password: {
+          type: DataTypes.STRING(6),
+          allowNull: true,
+        },
+      },
+      {
+        tableName: 'Room',
+        modelName: 'rooms',
+        timestamps: true,
+        sequelize,
+      },
+    );
+  }
+}
