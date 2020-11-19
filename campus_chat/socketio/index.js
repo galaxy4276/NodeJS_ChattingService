9916b450b150746b154059d1a7f27ed952eb1671
galaxy <galaxyhi4276@gmail.com>
2020-11-19 09:13:09 +0900

    feat: namespaces(socket.io) 추가

--- campus_chat/socketio/index.js
+++ campus_chat/socketio/index.js
@@ -3,6 +3,9 @@ import socketIO from 'socket.io';
 const serverSocket = (server, app) => {
   const io = socketIO(server);
   app.set('io', io);
+
+  const room = io.of('room');
+  const chat = io.of('chat');
   io.on('connection', () => {
     console.log('새 유저가 입장하였어요~');
   });
